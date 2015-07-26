34477335382e53f7662fa1fb77b7c0e44b6db9b8
jaredcobb <jaredcobb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-26 01:27:18 +0000

    Adds group address, longitude, and latitude as custom fields
    git-svn-id: https://plugins.svn.wordpress.org/church-community-builder-core-api/tags/0.9.4@1206581 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-ccb-core-activator.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://jaredcobb.com/ccb-core
+ * @since      0.9.0
+ *
+ * @package    CCB_Core
+ * @subpackage CCB_Core/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.9.0
+ * @package    CCB_Core
+ * @subpackage CCB_Core/includes
+ * @author     Jared Cobb <wordpress@jaredcobb.com>
+ */
+class CCB_Core_Activator extends CCB_Core_Plugin {
+
+	/**
+	 * Activation code
+	 *
+	 * @since    0.9.0
+	 */
+	public static function activate() {
+		// TODO: check dependencies like mcrypt and memory limits
+	}
+
+}
